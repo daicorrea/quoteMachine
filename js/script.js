05e0f84e680cfef3d96d653620bd0a629b3a6561
daiCorrea <daiane.ocorrea@gmail.com>
2017-05-22 16:19:03 -0300

    Getting Quote from API

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
-    $('.get-quote').on('click', function() {
-        $('.quote').html("new quote");
-    });
+$("document").ready(function() {
+  $('.get-quote').on('click', function(json) {
+    $('.quote').html("new quote");
+    var corsServer = "https://cors-anywhere.herokuapp.com/";
+    apiUrl = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
+    $.ajaxSetup( { cache:false} );
+    $.getJSON( corsServer + apiUrl, jsonAPIFormat, 'jsonp' );
+  });
 });
+
+function jsonAPIFormat(json) {
+  console.log(json[0].content);
+}
