3ae42a43851fdb23bb99cf3a7c6e087ae257c293
daiCorrea <daiane.ocorrea@gmail.com>
2017-05-22 16:23:06 -0300

    Showing Author and Quote

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 $("document").ready(function() {
   $('.get-quote').on('click', function(json) {
-    $('.quote').html("new quote");
     var corsServer = "https://cors-anywhere.herokuapp.com/";
     apiUrl = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
     $.ajaxSetup( { cache:false} );
@@ -9,5 +8,6 @@ $("document").ready(function() {
 });
 
 function jsonAPIFormat(json) {
-  console.log(json[0].content);
+    $('#quote-text').html(json[0].content);
+    $('#quote-author').html(json[0].title);
 }
